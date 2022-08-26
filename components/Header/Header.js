cf16141caba0eef8cf99b2272fd3fa678a53b0ee
Kamran Ansari <crossthinking786@gmail.com>
2022-08-26 16:20:08 +0530

    replace brand text with icons

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,27 +1,31 @@
 import Styles from './Header.module.css';
 import { useState } from 'react';
-import NavBar from './NavBar/NavBar';
 import MenuButton from './MenuButton/MenuButton';
 import MobileNavBar from './MobileNavBar/MobileNavBar';
 import DesktopNavBar from './DesktopNavBar/DesktopNavBar';
+import Image from 'next/image';
+import arganonLogoIcon from '../../public/organon_logo_icon.png';
+import arganonLogoText from '../../public/organon_logo_text.png';
 
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <header className={Styles.header}>
-      <h1 className={Styles.headingContainer}>
-        <div className={Styles.imageContainer}>
-          <picture>
-            <img
-              className={Styles.image}
-              src="/organon_logo.jpg"
-              alt="Organon Logo"
-            />
-          </picture>
-        </div>
-        <span>ORGANON</span>
-      </h1>
+      <div className={Styles.headingContainer}>
+        <Image
+          height={70}
+          width={70}
+          src={arganonLogoIcon}
+          alt="Arganon Logo Icon"
+        />
+        <Image
+          src={arganonLogoText}
+          height={70}
+          width={250}
+          alt="Arganon Logo Text"
+        />
+      </div>
 
       <MenuButton
         isOpen={isOpen}
