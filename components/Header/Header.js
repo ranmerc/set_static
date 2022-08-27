a757e2fe2daedd150a1d442b769f516a7c96f942
Kamran Ansari <crossthinking786@gmail.com>
2022-08-27 19:41:42 +0530

    use next/link in header navbar

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,14 +1,27 @@
 import Styles from './Header.module.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import MenuButton from './MenuButton/MenuButton';
 import MobileNavBar from './MobileNavBar/MobileNavBar';
 import DesktopNavBar from './DesktopNavBar/DesktopNavBar';
 import Image from 'next/image';
 import arganonLogoIcon from '../../public/organon_logo_icon.png';
 import arganonLogoText from '../../public/organon_logo_text.png';
+import { useRouter } from 'next/router';
 
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = () => {
+      setIsOpen(false);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <header className={Styles.header}>
--- components/Header/NavLinks/NavLink.js
+++ components/Header/NavLinks/NavLink.js
@@ -1,16 +1,18 @@
 import { useRouter } from 'next/router';
 import Styles from './NavLink.module.css';
+import Link from 'next/link';
 
 export default function NavLink({ href, children }) {
   const router = useRouter();
 
   return (
-    <a
-      aria-current={router.pathname === href ? 'page' : false}
-      className={Styles.link}
-      href={href}
-    >
-      {children}
-    </a>
+    <Link href={href}>
+      <a
+        aria-current={router.pathname === href ? 'page' : false}
+        className={Styles.link}
+      >
+        {children}
+      </a>
+    </Link>
   );
 }
