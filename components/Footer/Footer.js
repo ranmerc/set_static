622e15e032d9c20115195bb13bd2326e5dc896cf
Kamran Ansari <crossthinking786@gmail.com>
2022-12-05 16:51:44 +0530

    removed newsletter from from footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -5,7 +5,15 @@ import Style from './Footer.module.css';
 export default function Footer() {
   return (
     <footer className={Style.footer}>
-      <NewsletterForm />
+      {/* <NewsletterForm /> */}
+      <div className={Style.details}>
+        <div>H SOCIETY</div>
+        <div>
+          <div>Yours friendly neighborhood hacker society</div>
+          <div>Come and HACK AWAY!</div>
+          <div>VIT Vellore</div>
+        </div>
+      </div>
       <FollowUs />
     </footer>
   );
