5b7fe8b952656c20e5ae05cd50826d0fa782f6ab
Kamran Ansari <crossthinking786@gmail.com>
2022-12-05 18:30:36 +0530

    move newsletter form to newsletter page

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,11 +1,9 @@
 import FollowUs from './FollowUs/FollowUs';
-import NewsletterForm from './NewsletterForm/NewsletterForm';
 import Style from './Footer.module.css';
 
 export default function Footer() {
   return (
     <footer className={Style.footer}>
-      {/* <NewsletterForm /> */}
       <div className={Style.details}>
         <div>H SOCIETY</div>
         <div>
--- pages/newsletter.js
+++ pages/newsletter.js
@@ -1,12 +1,16 @@
 import Head from 'next/head';
+import NewsletterForm from '../components/NewsletterForm/NewsletterForm';
+import Styles from '../styles/newsletter.module.css';
 
 export default function Newsletter() {
   return (
     <>
       <Head>
-        <title>Organon - Newsletter</title>
+        <title>H Society - Newsletter</title>
       </Head>
-      <p>Hello Newsletter!</p>
+      <main className={Styles.container}>
+        <NewsletterForm />
+      </main>
     </>
   );
 }
