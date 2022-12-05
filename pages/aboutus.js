a0196ea43ceb06ab104a38e2328def5ec5613a7b
Kamran Ansari <crossthinking786@gmail.com>
2022-12-05 18:10:36 +0530

    updated aboutus page

--- pages/aboutus.js
+++ pages/aboutus.js
@@ -2,48 +2,49 @@ import Head from 'next/head';
 import Styles from '../styles/aboutus.module.css';
 import PageHeading from '../components/PageHeading/PageHeading';
 import Image from 'next/image';
-import iimrLogo from '../public/IIM-Rohtak2.png';
-import organonLogo from '../public/bulbdp_lowres.png';
+import vitLogo from '../public/VIT_logo.png';
+import hSocietyLogo from '../public/club_logo.png';
 
 export default function AboutUs() {
   return (
     <>
       <Head>
-        <title>Organon - About Us</title>
+        <title>H Society - About Us</title>
       </Head>
       <main className={Styles.container}>
         <PageHeading className={Styles.heading}>ABOUT US</PageHeading>
         <div className={Styles.paraContainer}>
           <p>
-            Analytics club is a student driven initiative of IIM-Rohtak, whose
-            basic idea is to generate and cultivate student interest in
-            analytics.
+            The primary goal of VIT Vellore&apos;s student-led H Society is to
+            spark and nurture students&apos; interest in ethical hacking and
+            hacktivism.
           </p>
           <p>
-            Our primary roles and responsibilities include conducting seminars
-            and webinars covering on-demand topics, skills and tools that will
-            help the IM Rohtak student&apos;s fraternity gain expertise over
-            various analytics tools.
+            Our main tasks and responsibilities include leading seminars and
+            webinars on demand-related subjects, abilities, and tools that will
+            benefit the VIT Vellore student fraternity&apos;s ability to master
+            various hacking tools.
           </p>
           <p>
-            Eventually these skills will help them during placements and even
-            during day-to-day work life in their respective fields. We organize
-            events such as Certification Drive, Competency Builder Events, Case
-            Study Competitions, Quiz Competitions and Online Treasure Hunt.
+            These abilities will eventually be useful to them both in their
+            field-specific postings and in their regular employment lives. Such
+            activities as Certification Drives, Competency Builder Events, Case
+            Study Competitions, Quiz Competitions, and Online Treasure Hunts are
+            organized by us.
           </p>
         </div>
         <div className={Styles.imageContainer}>
           <Image
-            src={iimrLogo}
-            width={130}
-            height={124}
-            alt="Indian Institute of Management Rohtak logo"
+            src={vitLogo}
+            width={125}
+            height={125}
+            alt="VIT Vellore logo"
           />
           <Image
             width={130}
             height={140}
-            src={organonLogo}
-            alt="Organon logo"
+            src={hSocietyLogo}
+            alt="H Society logo"
           />
         </div>
       </main>
