122c36b34ba4c3c398f9a0758b772903f29f3a1c
Kamran Ansari <crossthinking786@gmail.com>
2022-12-06 08:28:45 +0530

    use client side redirect on index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
 import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 export default function Home() {
+  let router = useRouter();
+
+  useEffect(() => {
+    router.replace('/events');
+  }, [router]);
+
   return (
     <div>
       <p>Ideally you wont&apos;t be seeing this page</p>
